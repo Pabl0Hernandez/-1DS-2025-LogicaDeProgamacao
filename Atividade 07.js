adc35eec5c59d2b0d352527ae23cafefadbbfe05
Pabl0Hernandez <pablo.hernandez@aluno.senai.br>
2025-02-21 10:29:25 -0300

    alteraçao do arquivo laco de repeticao

--- Atividade 07.js
+++ Atividade 07.js
@@ -8,6 +8,7 @@ Dada a matriz abaixo, faça um programa em JavaScript que exiba ao usuário os e
 |  7  1  4  |
 */
 
+//********** O meu **********
 var matriz = [
   [3, 5, 8],
   [1, 9, 8],
@@ -15,8 +16,58 @@ var matriz = [
 ];
 
 for (let linha = 0; linha < matriz.length; linha++) {
-    for (let coluna = 0; coluna < matriz[0].length; coluna++)
-      console.table(matriz);
-    console.log(matriz[0][0], "|", matriz[1][1], "|", matriz[2][2]);
+  for (let coluna = 0; coluna < matriz[0].length; coluna++)
+    console.table(matriz);
+  console.log(matriz[0][0], "|", matriz[1][1], "|", matriz[2][2]);
+}
+
+//********** O do professor **********
+var matriz = [
+  [3, 5, 8],
+  [1, 9, 8],
+  [7, 1, 4],
+];
+for (let linha = 0; linha < matriz.length; linha++) {
+  for (let coluna = 0; coluna < matriz[0].length; coluna++) {
+    linha == coluna &&
+      console.log(
+        `Matriz[][] = ${linha}][${coluna}] = ${matriz[linha][coluna]}`
+      );
   }
-  
+}
+
+/*
+Dada a matriz abaixo, faça um programa em JavaScript que multiplique seus elementos por um valor x e exiba ao usuário a matriz antes e após a multiplicação
+|  3  5  8  |
+|  1  9  2  |
+|  7  1  4  |
+*/
+
+var matriz = [
+  [3, 5, 8],
+  [1, 9, 2],
+  [7, 1, 4],
+];
+
+var x = 0;
+console.table(matriz);
+
+for (let linha = 0; linha < matriz.length; linha++) {
+  for (let coluna = 0; coluna < matriz[0].length; coluna++) {
+    matriz[linha][coluna] *= x;
+  }
+}
+
+console.table(matriz);
+
+// Faça um loop FOR onde apresente os números ímpares de 0 a 100;
+// Não pode ser resolvido com conta ou resto de divisão;
+console.log("Numeros impares de 0 a 100");
+for (let i = 1; i <= 100; i += 2) {
+  console.log(i);
+}
+
+console.log("Numeros pares de 0 a 100");
+for (let i = 0; i <= 100; i += 2) {
+  console.log(i);
+}
